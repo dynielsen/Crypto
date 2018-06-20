8301593382e1c311db1bb819c05fe094535008c8
Daniel <dynielsen1@hotmail.com>
2018-06-20 16:28:42 +0200

    added lightstreams and tolar

---
+++ ICO.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 16:20:18 2018
+
+@author: dnielsen
+"""
+
+# Info on crypto ICOinvestments
+
+ICO1='Lightstreams'
+ICO1_contribution={'Hooshang':1.5, 'Moonhee':1.3, 'Daniel':1.5}
+
+ICO2='Tolar'
+ICO2_contribution={'Hooshang':1.505, 'Moonhee':1.01, 'Daniel':1.5}
